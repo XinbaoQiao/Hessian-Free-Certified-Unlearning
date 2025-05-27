8db1d660cbf05c4cac5993ba87b221c1b25328ac
Mr. Ciao <86242730+XinbaoQiao@users.noreply.github.com>
2025-05-27 19:57:36 +0800

    Delete utils/test.py

--- utils/test.py
+++
@@ -1,6 +0,0 @@
-
-
-import torch
-print(torch.cuda.is_available())  # 检查是否可以使用 GPU
-print(torch.cuda.current_device())  # 显示当前使用的 GPU 设备索引
-print(torch.cuda.get_device_name(torch.cuda.current_device()))  # 显示当前 GPU 的名称
